db2588bf2c9aabe356a61d819f8fe1e1b9fddfc4
LeonardoCesca <leonardocescaf@gmail.com>
2017-09-03 11:02:33 -0300

    lista encadeada

---
+++ Aula4/src/App.java
@@ -0,0 +1,15 @@
+
+public class App {
+    public static void main(String[] args) {
+        LinkedListOfInteger l = new LinkedListOfInteger();
+        l.add(35);
+        l.add(0,90);
+        l.add(1,41);
+        l.add(2,26);
+        l.add(78);
+        l.add(10);
+        System.out.println(l);
+      
+        
+    }
+}
